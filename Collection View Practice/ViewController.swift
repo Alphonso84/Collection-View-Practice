04ec0494aa8efd6357c67eb481675311e643b6ea
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-05-07 21:20:01 -0700

    Initial Commit

---
+++ Collection View Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collection View Practice
+//
+//  Created by user on 5/7/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
